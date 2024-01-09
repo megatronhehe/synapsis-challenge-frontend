5d8e6a141983d9b82d7bf0f77f23943a895f5805
Megatronhehe <satyamahendra09@gmail.com>
2024-01-09 14:50:36 +0700

    [refactor] blogDetails and myBlogs routes changed to blog-details and my-blogs

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -11,8 +11,8 @@ export default function BlogCard({ blog }: BlogCardProps) {
 
 	return (
 		<Link
-			href={`/blogDetails/${id}?user_id=${user_id}`}
-			className="border text-sm p-4 h-32 rounded-xl flex flex-col justify-between"
+			href={`/blog-details/${id}?user_id=${user_id}`}
+			className="border text-sm p-4 h-32 rounded-xl flex flex-col justify-between shadow-sm"
 		>
 			<h2 className="">{title}</h2>
 			<div className="self-end text-xs">read more</div>
