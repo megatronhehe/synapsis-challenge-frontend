43bd45d310cff376f47e3542ade85dab6f3bd5e2
Megatronhehe <satyamahendra09@gmail.com>
2024-01-09 14:58:05 +0700

    [style] select/hover animation on blog card

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -12,10 +12,12 @@ export default function BlogCard({ blog }: BlogCardProps) {
 	return (
 		<Link
 			href={`/blog-details/${id}?user_id=${user_id}`}
-			className="border text-sm p-4 h-32 rounded-xl flex flex-col justify-between shadow-sm"
+			className="border text-sm p-4 h-32 rounded-xl flex flex-col justify-between shadow-sm hover:rounded-none duration-200 hover:border-blue-400 group"
 		>
 			<h2 className="">{title}</h2>
-			<div className="self-end text-xs">read more</div>
+			<div className="self-end text-xs group-hover:mr-0 mr-2 duration-200 ">
+				read more
+			</div>
 		</Link>
 	);
 }
