ade0507a0356bb9f51c7fad19f8472e7c459d732
Megatronhehe <satyamahendra09@gmail.com>
2024-01-11 20:30:38 +0700

    [style] more centered placement for blog details loading page

--- src/app/blog-details/[id]/loading.tsx
+++ src/app/blog-details/[id]/loading.tsx
@@ -2,7 +2,7 @@ import { PiCircleNotch } from "react-icons/pi";
 
 export default function loading() {
 	return (
-		<div className="flex  items-center sm:pt-40 text-2xl min-h-screen text-gray-400 flex-col gap-6">
+		<div className="flex  items-center pt-40 text-2xl min-h-screen text-gray-400 flex-col gap-6">
 			<PiCircleNotch className="animate-spin" /> loading blog...
 		</div>
 	);
