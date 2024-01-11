3d93023efd045f7b7232785e3977e2284482a8da
Megatronhehe <satyamahendra09@gmail.com>
2024-01-11 20:25:47 +0700

    [style] better blog skeleton desgin

--- src/components/skeletons/BlogsListSkeleton.tsx
+++ src/components/skeletons/BlogsListSkeleton.tsx
@@ -6,8 +6,16 @@ export default function BlogsListSkeleton() {
 	const CardSkeleton = array.map((item) => (
 		<div
 			key={item}
-			className="border text-sm p-4 h-32 rounded-xl flex flex-col justify-between shadow-sm hover:rounded-none bg-gray-100 animate-pulse duration-200 hover:border-blue-400 group"
-		></div>
+			className="border text-sm p-4 h-32 rounded-xl flex flex-col justify-between shadow-sm text-gray-200 bg-gray-100 animate-pulse "
+		>
+			<div className="flex flex-col gap-1">
+				<div className="w-full bg-gray-200 rounded-lg ">a</div>
+				<div className="w-1/3 text-gray-200 bg-gray-200 rounded-lg">a</div>
+			</div>
+			<div className="self-end text-xs w-1/3 flex items-center justify-center gap-1 bg-gray-200 rounded-xl">
+				a
+			</div>
+		</div>
 	));
 
 	return (
