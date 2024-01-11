4e466ac4be7f0830e11af4f0d26846bc8c7b94c8
Megatronhehe <satyamahendra09@gmail.com>
2024-01-11 14:16:48 +0700

    [feature] error handling and searched users not found on users page

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -2,6 +2,7 @@ import UserItem from "./UserItem";
 
 import { UserType } from "@/types/UserType";
 import { getUsers } from "@/actions/users";
+import ReloadButton from "./buttons/ReloadButton";
 
 type UsersListProps = {
 	searchParams?: { query?: string };
@@ -16,6 +17,25 @@ export default async function UsersList({
 	await new Promise((resolve) => setTimeout(resolve, 500));
 
 	const users = await getUsers(searchParams?.query || "", currentPage || 1);
+
+	// error
+	if (!users) {
+		return (
+			<div className="my-40 flex items-center flex-col gap-4 justify-center">
+				<h1>Something went wrong...</h1>
+				<ReloadButton />
+			</div>
+		);
+	}
+
+	// if no users found
+	if (users.length < 1) {
+		return (
+			<div className="text-center my-24 text-gray-400">
+				No user with the name '{searchParams?.query}' was found...
+			</div>
+		);
+	}
 
 	return (
 		<ul className="flex flex-col gap-2 -mb-4">
