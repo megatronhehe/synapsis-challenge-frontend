3972b1eed9c81cc5577cf02cde93145dfbf1d4b2
Megatronhehe <satyamahendra09@gmail.com>
2024-01-11 11:13:25 +0700

    [style] responsive navbar button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,13 @@
+"use client";
+
+import { PiHouse, PiHouseFill, PiUser, PiUserFill } from "react-icons/pi";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+	const pathname = usePathname();
+
 	return (
 		<header className="font-light text-gray-600 border-b">
 			<nav className="flex justify-center">
@@ -11,12 +18,36 @@ export default function Navbar() {
 							<span className="font-semibold text-blue-400">.Blogs</span>
 						</Link>
 					</li>
-					<div className="flex gap-6">
+					<div className="flex gap-6 text-base">
 						<li>
-							<Link href="/">Home</Link>
+							<Link
+								href="/"
+								className={`pb-2 duration-200 ${
+									pathname === "/" ? " text-blue-400 font-bold" : ""
+								}`}
+							>
+								{pathname === "/" ? (
+									<PiHouseFill className="block sm:hidden text-2xl" />
+								) : (
+									<PiHouse className="block sm:hidden text-2xl" />
+								)}
+								<span className="hidden sm:block">Home</span>
+							</Link>
 						</li>
 						<li>
-							<Link href="/users">Users</Link>
+							<Link
+								href="/users"
+								className={`pb-2 duration-200 ${
+									pathname === "/users" ? " text-blue-400 font-bold" : ""
+								}`}
+							>
+								{pathname === "/users" ? (
+									<PiUserFill className="block sm:hidden text-2xl" />
+								) : (
+									<PiUser className="block sm:hidden text-2xl" />
+								)}
+								<span className="hidden sm:block">Users</span>
+							</Link>
 						</li>
 					</div>
 				</ul>
